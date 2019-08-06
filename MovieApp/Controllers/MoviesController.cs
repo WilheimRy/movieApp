2fac519bb50f1b33479e2ce48efbe20f996b5c26
WilheimRy <wilheim.ry@gmail.com>
2019-08-06 21:37:08 +1200

    Initial commit

---
+++ MovieApp/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieApp.Models;
+
+namespace MovieApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Princes Laya"};
+            //return View(movie);
+
+            return Content("This is the content message");
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            return Content($"id= {id}");
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "default";
+
+            return Content($"pageindex= {pageIndex}, sortby= {sortBy}");
+        }
+
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year,int month)
+        {
+            return Content($"year = {year}, month = {month}");
+        }
+    }
+}
